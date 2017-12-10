d57ca805569ce4b752cc02a35dba2239dfb494b7
chauhansups <singhsupriya12794@gmail.com>
2017-12-10 14:08:45 +0530

    Add files via upload

---
+++ swapingnowith3rdvariable.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class swapingnowith3rdvariable {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
