96992cbc8a5ae1efba1269be65dff058907a7a2f
chauhansups <singhsupriya12794@gmail.com>
2017-12-10 14:07:35 +0530

    Add files via upload

---
+++ Calculations.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Calculations {
+	public static void main(String []args)
+	{
+	int a=225;
+	int b=125;
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a/b);
+	System.out.println(a%b);
+	System.out.println(a*b);
+	
+
+}
+}
